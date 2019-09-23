7672e102b1475ebd704b07c2cb9e8ca12244cb82
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-23 23:25:32 +0530

    Updated

--- First/ApplicationTest/app/src/main/java/com/android/anothertutorial/MainActivity.java
+++ First/ApplicationTest/app/src/main/java/com/android/anothertutorial/MainActivity.java
@@ -3,17 +3,20 @@ package com.android.anothertutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer sound;
     TextView tvLoading;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvLoading=(TextView) findViewById(R.id.tvLoading);
+        sound=MediaPlayer.create(MainActivity.this,R.raw.music);
+        sound.start();
         Thread timer=new Thread(){
           public void run(){
               try {
@@ -32,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        sound.release();
         finish();
     }
 }
